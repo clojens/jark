5f2ea88291c327755ffcc6399f227457e08966db
Isaac Praveen <icylisper@gmail.com>
2012-09-02 10:30:08 +0530

    Added lib

---
+++ lib/nrepl/nrepl.ml
@@ -0,0 +1,100 @@
+(*pp $PP *)
+
+
+(* The Nrepl module is written by Martin Demello *) 
+(* Ref: https://github.com/martindemello/ocaml-nrepl-client.git *)
+
+module Nrepl =
+  struct
+    
+    open Printf
+    open Ntypes
+    open Gstr  
+
+    let debug = ref false
+
+    let debugging x =
+      if !debug then print_endline x;
+      x
+
+    let new_response = {
+      id     = None;
+      out    = None;
+      err    = None;
+      value  = None;
+      status = None;
+    }
+
+    let bad_response =
+      {new_response with err = Some("Bad response from server")}
+
+    let empty_response = 
+      {new_response with err = Some("Empty response from server")}
+
+    let update_response res (x, y) =
+      let y = Some (Gstr.uq y) in
+      match (Gstr.uq x) with
+      | "id"     -> {res with id = y};
+      | "out"    -> {res with out = y};
+      | "err"    -> {res with err = y};
+      | "value"  -> {res with value = y};
+      | "status" -> {res with status = y};
+      | _        -> res (* TODO: raise malformed response *)
+
+    type state = NewPacket | Receiving of int | Done
+
+    let readlines socket =
+      let input = Unix.in_channel_of_descr socket in
+      let getline () = debugging (try input_line input with End_of_file -> "") in
+      let concat xs = match xs with
+      | [] -> None
+      | _  -> Some (String.concat "" (List.map Gstr.us (List.rev xs)))
+      in
+      let value = ref None in
+      let out = ref [] in
+      let err = ref [] in
+      let rec get s res =
+        match s with
+        | NewPacket ->
+            let line = getline () in
+            begin
+              match (line, Gstr.maybe_int line) with
+              | "", _     -> empty_response
+              | _, None   -> bad_response
+              | _, Some i ->  get (Receiving i) new_response
+            end
+        | Done ->
+            {res with value = !value; out = concat !out; err = concat !err}
+        | Receiving 0 ->
+            if Gstr.notnone res.err then err := res.err :: !err;
+            if Gstr.notnone res.out then out := res.out :: !out;
+            if Gstr.notnone res.value then value := res.value;
+            get NewPacket res
+        | Receiving n ->
+            let k = getline () in
+            let v = getline () in
+            let res = update_response res (k, v) in
+            match res.status with
+            | Some "done"  -> get Done res
+            | _            -> get (Receiving (n - 1)) res
+            in
+            get NewPacket new_response
+
+    let write_all socket s =
+      Unix.send socket s 0 (String.length s) []
+
+    let nrepl_message_packet msg =
+      ["2"; Gstr.q "id"; Gstr.q msg.mid; Gstr.q "code"; Gstr.q msg.code]
+
+    let send_msg env msg =
+      let socket = Unix.socket Unix.PF_INET Unix.SOCK_STREAM 0 in
+      let hostinfo = Unix.gethostbyname env.host in
+      let server_address = hostinfo.Unix.h_addr_list.(0) in
+      let _ = Unix.connect socket (Unix.ADDR_INET (server_address, env.port)) in
+      let msg = Gstr.unlines (nrepl_message_packet msg) in
+      let _ = debug := env.debug in
+      let _ = write_all socket msg in
+      let res = readlines socket in
+      Unix.close socket;
+      res
+  end
---
+++ lib/nrepl/response.ml
@@ -0,0 +1,95 @@
+module Response =
+  struct
+
+    open Ntypes
+    open Gstr
+
+    (* helper functions for response deserialization *)
+    let rq x = Str.regexp (Str.quote x)
+
+    let rx x = Str.regexp x
+
+    let remove_rx r x = Str.global_replace (rx r) "" x
+
+    let uq_list x = List.map Gstr.uq x
+
+    let kv_split x = Str.bounded_split (rx ":") x 2
+
+    (* nrepl response idiosyncrasies *)
+    let regenerate_newline str = Str.global_replace (rx "\\\\n") "\n" str
+
+    let nilp str = 
+      (Gstr.strip (regenerate_newline str)) = "nil"
+
+    (* format strings from response fields *)
+    let unescape x =
+      let q = rq "\\\"" in
+      let db = rq "\\\\" in
+      let x = Str.global_replace q "\"" x in
+      let x = Str.global_replace db "\\\\" x in
+      x
+
+    let fmt_txt x =
+      let x = regenerate_newline (Gstr.us x) in
+      unescape x
+
+    (* handle stringified file lists and hashes in response *)
+    let deserialize_common x =
+      let x = unescape x in
+      let x = Gstr.strip x in
+      let x = Gstr.uq x in
+      let fs = rq "\\/" in
+      let x = Str.global_replace fs "/" x in
+      x
+
+    let deserialize_list x =
+      let x = deserialize_common x in
+      let x = Gstr.strip ~chars:"\\[]" x in
+      let xs = Gstr.split "," x in
+      Gstr.unlines (List.map Gstr.uq xs)
+
+    let deserialize_hash x =
+      let x = deserialize_common x in
+      let x = Gstr.strip ~chars:"\\{}" x in
+      let xs = Gstr.split "," x in
+      let kvs = List.map kv_split xs in
+      let kvs = List.map uq_list kvs in
+      Gstr.unlines (List.map (String.concat " : ") kvs)
+
+    (* res.value needs "nil" and datastructures to be special-cased *)
+    let fmt_val x ?(fmt=ResText) nilv =
+      let v = fmt_txt x in
+      if (nilp v) then nilv
+      else match fmt with
+        ResText -> v
+      | ResList -> deserialize_list v
+      | ResHash -> deserialize_hash v
+
+    let make_res_string out value res =
+      let (>>?) x y = if x then [y] else [] in
+      let value_format = ("=> " ^ Gstr.strip ~chars:"\n" (fmt_val res.value "nil")) in
+      if (Gstr.notnone res.out) then
+        (fmt_txt res.out) ^ value_format
+      else
+        value_format
+
+    (* called by `eval` in the repl
+     * NOTE: unlike clojure's repl, we put a newline between out and val
+     * even if out is not \n-terminated *)
+    let string_of_res ?(out=true) ?(value=true) res =
+      match res.err with
+          Some e -> fmt_txt res.err
+        | None   -> make_res_string out value res
+
+    (* output err or (both out and value) *)
+    let print_res ?(fmt=ResText) res =
+      if (Gstr.notnone res.err) then
+        print_endline (fmt_txt res.err)
+      else
+        begin
+          Gstr.println_unless_empty (Gstr.strip ~chars:"\n" (fmt_txt res.out));
+          Gstr.println_unless_empty (fmt_val res.value ~fmt:fmt "")
+        end;
+      flush stdout
+
+  end
